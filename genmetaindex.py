885018fbdd568561418799cef54b10af91d7702a
jbleyel <jbleyel@gmx.net>
2023-02-17 19:48:33 +0100

    genmetaindex.py merge from master

--- genmetaindex.py
+++ genmetaindex.py
@@ -1,28 +1,26 @@
 # usage: genmetaindex.py <xml-files>  > index.xml
-import sys, os
-import six
-
+import sys
+import os
 from xml.etree.ElementTree import ElementTree, Element
-
 root = Element("index")
 
 for file in sys.argv[1:]:
 	p = ElementTree()
 	p.parse(file)
-	
+
 	package = Element("package")
 	package.set("details", os.path.basename(file))
-	
+
 	# we need all prerequisites
 	package.append(p.find("prerequisites"))
-	
+
 	info = None
 	# we need some of the info, but not all
 	for i in p.findall("info"):
 		if not info:
 			info = i
 	assert info
-	
+
 	for i in info[:]:
 		if i.tag not in ["name", "packagename", "packagetype", "shortdescription"]:
 			info.remove(i)
@@ -32,24 +30,26 @@ for file in sys.argv[1:]:
 
 	root.append(package)
 
+
 def indent(elem, level=0):
-	i = "\n" + level*"\t"
+	i = "\n" + level * "\t"
 	if len(elem):
 		if not elem.text or not elem.text.strip():
 			elem.text = i + "\t"
 		if not elem.tail or not elem.tail.strip():
 			elem.tail = i
 		for elem in elem:
-			indent(elem, level+1)
+			indent(elem, level + 1)
 		if not elem.tail or not elem.tail.strip():
 			elem.tail = i
 	else:
 		if level and (not elem.tail or not elem.tail.strip()):
 			elem.tail = i
 
+
 indent(root)
 
-if six.PY3:
+if sys.version_info[0] == 3:
 	ElementTree(root).write(sys.stdout, encoding='unicode')
-elif six.PY2:
+else:
 	ElementTree(root).write(sys.stdout)
